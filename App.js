bf593a17c6d57480e9572d429ca4a4e9e9c3a125
poojasf13 <poojasf1316@gmail.com>
2023-03-10 15:18:14 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,91 @@
+import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  // Image,
+  navigation,
+} from 'react-native';
+import Login from './Screens/Login';
+import Home from './Screens/Home';
+const Stack = createNativeStackNavigator();
+export default function App() {
+ 
+  return ( 
+       <NavigationContainer>
+         <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+       </NavigationContainer> 
+  );
+}
+// eslint-disable-next-line no-shadow
+// const HomeScreen = ({navigation}) => {
+//   return (
+//     <Button
+//       title="Go to Jane's profile"
+//       onPress={() => navigation.navigate('Profile', {name: 'Jane'})}
+//     />
+//   );
+// };
+const ProfileScreen = ({navigation, route}) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    marginBottom: 40,
+  },
+  text: {
+    fontSize: 30,
+    paddingBottom: 50,
+    color: 'black',
+  },
+  inputView: {
+    backgroundColor: 'white',
+    borderRadius: 30,
+    width: '70%',
+    height: 45,
+    marginBottom: 20,
+
+    borderColor: 'black',
+  },
+  TextInput: {
+    height: 50,
+    paddingLeft: 20,
+    marginLeft: 0,
+    borderWidth: 1,
+    borderRadius: 50,
+    borderColor: 'black',
+  },
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+  loginBtn: {
+    width: '80%',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+});
---
+++ Screens/Login.js
@@ -0,0 +1,101 @@
+import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  // Image,
+  navigation,
+} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+import Home from './Home';
+export default function Login(navigation) {
+  const [setEmail] = useState('');
+  const [setPassword] = useState('');
+  return (
+    
+      <View style={styles.container}>
+              <Text style={styles.text}>LogIn</Text>
+              <View style={styles.inputView}>
+                  <TextInput
+                      style={styles.TextInput}
+                      placeholder="Email."
+                      placeholderTextColor="#003f5c"
+                      onChangeText={email => setEmail(email)} />
+              </View>
+              <View style={styles.inputView}>
+                  <TextInput
+                      style={styles.TextInput}
+                      placeholder="Password."
+                      placeholderTextColor="#003f5c"
+                      secureTextEntry={true}
+                      onChangeText={password => setPassword(password)} />
+              </View>
+              <TouchableOpacity>
+                  <Text style={styles.forgot_button}>Forgot Password?</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.loginBtn}  
+               onPress={() => navigation.navigate(Home)}>
+                  <Text style={styles.loginText}>LOGIN</Text>
+              </TouchableOpacity>
+                 
+          </View>
+    
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    marginBottom: 40,
+  },
+  text: {
+    fontSize: 30,
+    paddingBottom: 50,
+    color: 'black',
+  },
+  inputView: {
+    backgroundColor: 'white',
+    borderRadius: 30,
+    width: '70%',
+    height: 45,
+    marginBottom: 20,
+
+    borderColor: 'black',
+  },
+  TextInput: {
+    height: 50,
+    paddingLeft: 20,
+    marginLeft: 0,
+    borderWidth: 1,
+    borderRadius: 50,
+    borderColor: 'black',
+  },
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+  loginBtn: {
+    width: '80%',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+});
